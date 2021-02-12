8da796abc637178b14104920bfc4d8472141999e
Oskari Mantere <osku.mantere@gmail.com>
2021-02-12 11:06:15 +0200

    some setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+function testee(x) {
+  if (x > 0)
+    console.log('hello world');
+  else
+    console.log('world hello');
+}
+
+module.exports = { testee };
---
+++ test.js
@@ -0,0 +1,6 @@
+describe('program', () => {
+  it('runs', () => {
+    const { testee } = require('./index');
+    testee(0);
+  });
+});
